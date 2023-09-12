e0d5fd5c4cfc074bc6565b468e45ceee009b5407
Badal Karki <badalkarki@Badals-MBP.lan>
2023-09-11 20:36:21 -0500

    code pushing to remote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [query, setQuery] = useState("");
+  const [response, setResponse] = useState("");
 
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); 
+    try {
+      const result = await fetch("http://127.0.0.1:5000/query", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query })
+      });
+      const data = await result.json();
+      setResponse(data.response);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setResponse("Error fetching data");
+    }
+  };
+  
+return (
+  <div className="App">
+    <div className="App-header">
+      <h2>Ask me anything?</h2>
+      <form onSubmit={handleSubmit} className="input-container">
+  <input 
+    type="text" 
+    value={query} 
+    onChange={handleChange} 
+    placeholder="Enter your query..." 
+  />
+  <button type="submit" disabled={!query.trim()}></button>
+</form>
+      <div className="response-section">
+        <div className="response-title">Response:</div>
+        <div className="response-text">{response}</div>
+      </div>
+    </div>
+  </div>
+);
+}
 export default App;
